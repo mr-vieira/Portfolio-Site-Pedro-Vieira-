8e7b5d3aaf7fdb6268e656e00647f98035a582b2
mr-vieira <vieira.dg@gmail.com>
2022-03-05 23:54:15 +0000

    add a fully functioning hamburger menu
    for screen sizes under 640px width.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
+
 console.log ("Hi! Welcome to My UX Portfolio Site")
+
+
+// Hamburger menuToggle
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
