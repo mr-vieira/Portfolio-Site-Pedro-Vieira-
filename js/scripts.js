4a731efd2740c09bcdf1a95795648653b3694d0a
mr-vieira <vieira.dg@gmail.com>
2022-03-09 11:50:13 +0000

    apply general layout and typesetting  adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,11 @@
-
-console.log ("Hi! Welcome to My UX Portfolio Site")
+console.log("Hi! Welcome to My UX Portfolio Site")
 
 
 // Hamburger menuToggle
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
