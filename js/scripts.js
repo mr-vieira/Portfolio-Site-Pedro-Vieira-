0094a01564b643049adfe4e6a46e9a82a94da803
mr-vieira <vieira.dg@gmail.com>
2022-03-10 17:19:40 +0000

    correct errors in HTML, CSS, and JavaScript code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-console.log("Hi! Welcome to My UX Portfolio Site")
-
+console.log('Hi! Welcome to My UX Portfolio Site')
 
 // Hamburger menuToggle
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle' && $(window).width() < 640) {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
