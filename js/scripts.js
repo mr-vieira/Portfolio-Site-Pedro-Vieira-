f3011959731d33eb78e63235694d562aa84e371b
mr-vieira <vieira.dg@gmail.com>
2023-01-13 14:08:53 +0000

    DAILY UI 004

--- js/scripts.js
+++ js/scripts.js
@@ -54,23 +54,24 @@ function topFunction () {
 
 // SLIDESHOW GRAPHIC Design
 
-var slideIndex = 0;
-carousel();
+var slideIndex = 0
+carousel()
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
+function carousel () {
+  var i
+  var x = document.getElementsByClassName('mySlides')
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = 'none'
   }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  slideIndex++
+  if (slideIndex > x.length) { slideIndex = 1 }
+  x[slideIndex - 1].style.display = 'block'
+  setTimeout(carousel, 2000) // Change image every 2 seconds
 }
 
+// SLIDESHOW Audio
 
-function play() {
-  var audio = document.getElementById("audio");
-  audio.play();
+function play () {
+  var audio = document.getElementById('audio')
+  audio.play()
 }
